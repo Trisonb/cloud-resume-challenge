a9f41e16ea3fae4348b84a214bfa83ca5b3bfc06
Trisonb <trisonb@gmail.com>
2025-08-03 03:59:11 -0400

    Editing text in the resume, adding symbols to matrix rain, fixing visit count sizes

--- cloudresume.js
+++ cloudresume.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const matrixRain = document.getElementById('matrix-rain');
 
     // Define the character set for the Matrix effect (letters, numbers, symbols)
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?~`';
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?~`§¶∆∑Ωµπ';
 
     // Create 24 columns with 18 characters each
     for (let i = 0; i < 24; i++) {
